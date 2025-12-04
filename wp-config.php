99c587d94fe0aca5860ba365c07cb9bc1dbc9f2b
Ralph Francois <ralph@studgate.com>
2025-12-03 19:50:18 -0500

    small sync with wp-engine version

--- wp-config.php
+++ wp-config.php
@@ -1,70 +1,79 @@
 <?php
 /**
- * This config file is yours to hack on. It will work out of the box on Pantheon
- * but you may find there are a lot of neat tricks to be used here.
+ * The base configuration for WordPress
  *
- * See our documentation for more details:
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
  *
- * https://pantheon.io/docs
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * Localized language
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
  */
 
- // Include for ddev-managed settings in wp-config-ddev.php.
-$ddev_settings = dirname(__FILE__) . '/wp-config-ddev.php';
-if (is_readable($ddev_settings) && !defined('DB_USER')) {
-  require_once($ddev_settings);
-}
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'local' );
 
+/** Database username */
+define( 'DB_USER', 'root' );
 
-/**
- * Pantheon platform settings. Everything you need should already be set.
- */
-elseif (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-	require_once(dirname(__FILE__) . '/wp-config-pantheon.php');
+/** Database password */
+define( 'DB_PASSWORD', 'root' );
 
-/**
- * Local configuration information.
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
  *
- * If you are working in a local/desktop development environment and want to
- * keep your config separate, we recommend using a 'wp-config-local.php' file,
- * which you should also make sure you .gitignore.
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
  */
-} elseif (file_exists(dirname(__FILE__) . '/wp-config-local.php') && !isset($_ENV['PANTHEON_ENVIRONMENT'])){
-	# IMPORTANT: ensure your local config does not include wp-settings.php
-	require_once(dirname(__FILE__) . '/wp-config-local.php');
+define( 'AUTH_KEY',          '=y3RF=lYWqX#c*uS2&O,fQ--}F{bt=E}Kzp6Na}odJS>@4d+z]KY6{Fti~r-tFk2' );
+define( 'SECURE_AUTH_KEY',   '(Bi].Kvc3z?FjhVVc_~?[tp`No)(a=t?k/a5lc10CFK$^GwXd<K]P0nK*)C1)1&s' );
+define( 'LOGGED_IN_KEY',     '0yh`]Zf}5[z)/n#+:c<KB2+aPyb#(}l8j*lErWseVey>n&-9|Xub7mgzXv)8+ALH' );
+define( 'NONCE_KEY',         '9y:5h8O|ZzD[FzGIbS,A:6x&wa1>YM!+pJk-]@$_mXq%ti`8Tf!Fta.L0D>ReNat' );
+define( 'AUTH_SALT',         '2VFr%w3@iN6 =j>Ezp+Va8!XOX7m51S9)TALz3!kX#?$ef4rtnTJhFMGSprGv5cz' );
+define( 'SECURE_AUTH_SALT',  '[i}Z?3)R)e~HjQ!Ri@Bomvf?Bxki*h39t.EW=u^JBz%H!`+tX;c4*}nrR$P;e*T?' );
+define( 'LOGGED_IN_SALT',    'EN|x.~sMc+L QdQVwh@CgUS,s3`1R2$.Vo$u hk5I8ad*~I4jl:^*XZ7l$Rrd7:_' );
+define( 'NONCE_SALT',        '{SpLH<Q0_H,}+| ^)P8hx,HJcm}6t3+-QP11@P-x#t)47U(JN<USF:,bl/I24,/ ' );
+define( 'WP_CACHE_KEY_SALT', '3 wOG2*B6N^`x%G}B1rqz*s[M=&$>Y)x##qz3M,Br{iblhsbK,Aw> O>?TLNt&6&' );
+
+
+/**#@-*/
 
 /**
- * This block will be executed if you are NOT running on Pantheon and have NO
- * wp-config-local.php. Insert alternate config here if necessary.
+ * WordPress database table prefix.
  *
- * If you are only running on Pantheon, you can ignore this block.
- */
-} else {
-	define('DB_NAME',          'database_name');
-	define('DB_USER',          'database_username');
-	define('DB_PASSWORD',      'database_password');
-	define('DB_HOST',          'database_host');
-	define('DB_CHARSET', 'utf8');
-	define('DB_COLLATE', 'utf8_unicode_ci');
-	define('AUTH_KEY',         'put your unique phrase here');
-	define('SECURE_AUTH_KEY',  'put your unique phrase here');
-	define('LOGGED_IN_KEY',    'put your unique phrase here');
-	define('NONCE_KEY',        'put your unique phrase here');
-	define('AUTH_SALT',        'put your unique phrase here');
-	define('SECURE_AUTH_SALT', 'put your unique phrase here');
-	define('LOGGED_IN_SALT',   'put your unique phrase here');
-	define('NONCE_SALT',       'put your unique phrase here');
-}
-
-
-/** Standard wp-config.php stuff from here on down. **/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix = 'wp_';
+
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
 
 /**
  * For developers: WordPress debugging mode.
@@ -73,26 +82,22 @@ $table_prefix = 'wp_';
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  *
- * You may want to examine $_ENV['PANTHEON_ENVIRONMENT'] to set this to be
- * "true" in dev, but false in test and live.
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 if ( ! defined( 'WP_DEBUG' ) ) {
-	define('WP_DEBUG', false);
+	define( 'WP_DEBUG', false );
 }
 
-define('WP_CACHE', true);
-define('DISALLOW_FILE_EDIT', false);
-define('DISALLOW_FILE_MODS', false);
-define('WPLANG', '');
-
-/* That's all, stop editing! Happy Pressing. */
-
-
-
+define( 'WP_ENVIRONMENT_TYPE', 'local' );
+/* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once ABSPATH . 'wp-settings.php';
